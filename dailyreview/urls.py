23d4f0e3d106c6a65bab5e9f4bf87a54bbe56653
Somya Bansal <somi.bansal159@gmail.com>
2022-12-04 23:14:23 +0530

    Updated friends.css and friends.html

--- dailyreview/urls.py
+++ dailyreview/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html")),
+    path("", TemplateView.as_view(template_name="friendship/friends.html")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("logout", LogoutView.as_view(), name="logout"),
