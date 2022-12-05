7041648fe2491367adc183753f998cee4f2fa5ee
Soumik Dutta <shalearkane@gmail.com>
2022-12-05 17:50:19 +0530

    added friendship apis
    Signed-off-by: Soumik Dutta <shalearkane@gmail.com>

--- friendship/models.py
+++ friendship/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Friendship(models.Model):
@@ -28,6 +29,7 @@ class Friendship(models.Model):
 
 
 class FriendshipRequests(models.Model):
+    is_cleaned = False
     from_user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -47,6 +49,22 @@ class FriendshipRequests(models.Model):
 
     created = models.DateTimeField(auto_now_add=True, editable=False)
     rejected = models.BooleanField(default=False)
+    accepted = models.BooleanField(default=False)
+
+    def clean(self):
+        self.is_cleaned = True
+        if self.rejected is True and self.accepted is True:
+            raise ValidationError(
+                "Both rejected and accepted cannot be True at the same time."
+            )
+        if self.to_user == self.from_user:
+            raise ValidationError("You cannot friend yourself")
+        super(FriendshipRequests, self).clean()
+
+    def save(self, *args, **kwargs):
+        if not self.is_cleaned:
+            self.full_clean()
+        super(FriendshipRequests, self).save(*args, **kwargs)
 
     class Meta:
         unique_together = ("from_user", "to_user")
--- friendship/urls.py
+++ friendship/urls.py
@@ -6,6 +6,16 @@ urlpatterns = [
     path("", views.MyFriendsView.as_view(), name="my-friends"),
     path("discover/", views.DiscoverFriendsView, name="discover-friends"),
     path(
-        "requests/", views.FriendshipRequestsView.as_view(), name="friendship-requests"
+        "requests/", views.FriendRequestsView.as_view(), name="friendship-requests"
+    ),
+    path(
+        "requests/<int:pk>",
+        views.ProcessFriendRequestView,
+        name="process-friendship-request",
+    ),
+    path(
+        "requests/send/<int:user_id>",
+        views.SendFriendshipRequestView,
+        name="send-friendship-request",
     ),
 ]
